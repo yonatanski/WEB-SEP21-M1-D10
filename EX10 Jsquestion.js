f7a1765b017a86141e28126bcb02b850a986820a
yonatanski <88892819+yonatanski@users.noreply.github.com>
2021-10-01 16:29:45 +0200

    Dom manuplation

--- EX10 Jsquestion.js
+++ EX10 Jsquestion.js
@@ -1,25 +1,96 @@
 // JS EXERCISES
 
 //         21) Given variables x = "John" and y = "Doe", write on the console "John <> Doe"
-            consol.log("\n----------------->Excercise 21 JS<-----------------")
+            console.log("\n----------------->Excercise 21 JS<-----------------")
             let x = "john"
             let y = " Doe"
             console.log( x + "<>" + y)
 //         22) Create an object with properties such name, surname, email
+console.log("\n----------------->Excercise 22 JS<-----------------")
+              let myObject ={
+                  name :"Yonatan",
+                  surname :"Deribe",
+                  emial: "phpcodeyg@gmail.com"
+              }
+              console.log('This "My Object" object',myObject)
+
 //         23) Delete the email property from the previously created object
+console.log("\n----------------->Excercise 23 JS<-----------------")
+     delete myObject.emial
+     console.log(`This "My Object" object after deleting the Email property:${myObject}`)
 //         24) Create an array with 10 strings in it
+console.log("\n----------------->Excercise 24 JS<-----------------")
+       let arrayTenString =["one","two","three","four","five","six","Seven","Eight","Nine","ten"]
+       console.log (arrayTenString)
+       // with for loop
+       let myArray = [];
+
+       for (let i = 0; i < 10; i++) {
+         myArray.push("New value: " + i);
+       }
+          // with for loop
+    
 //         25) Print in the console every string from the previous array
-//         26) Create an array with 100 random numbers in it
+console.log("\n----------------->Excercise 25 JS<-----------------")
+    for (let i = 0; i < arrayTenString.length; i++) {
+        text = arrayTenString[i] + "<br>";
+        console.log(text)
+    }
+  
+      //         26) Create an array with 100 random numbers in it
+      console.log("\n----------------->Excercise 26 JS<-----------------")
+      let randomNumbers = [];
+      for (let i = 0; i < 100; i++) {
+        randomNumbers.push(Math.round(Math.random() * 100));
+      }
+
+      console.log("Here its the 100 random Array",randomNumbers);
 //         27) Write a function to get the maximum and minimum values from the previously created array
+console.log("\n----------------->Excercise 27 JS<-----------------")
+        // Bit cpomplicated
+          // Bit cpomplicated
+            // Bit cpomplicated
+            console.log("\n----------------->I will figer out later may be<-----------------")
+
+
+
 //         28) Create an array of arrays, in which every array has 10 random numbers
+console.log("\n----------------->Excercise 28 JS<-----------------")
+let arrayOfArrays = [];
+
+      for (let i = 0; i < 10; i++) {
+        let innerArray = [];
+        for (let j = 0; j < 10; j++) {
+          innerArray.push(Math.round(Math.random() * 10));
+        }
+        arrayOfArrays.push(innerArray);
+      }
+
+      console.log(arrayOfArrays)
 //         29) Create a function that gets 2 arrays as parameters and returns the longest one
+console.log("\n----------------->Excercise 28 JS<-----------------")
+    // Bit cpomplicated
+          // Bit cpomplicated
+            // Bit cpomplicated
+            console.log("\n----------------->I will figer out later may be<-----------------")
 //         30) Create a function that gets 2 arrays of numbers as parameters and returns the one with the higher sum of values
+console.log("\n----------------->Excercise 28 JS<-----------------")
+  // Bit cpomplicated
+          // Bit cpomplicated
+            // Bit cpomplicated
+            console.log("\n----------------->I will figer out later may be<-----------------")
 
 //         DOM EXERCISES
+console.log("\n-----------------> DOM EXERCISES<-----------------")
 
 //         31) Get the element with an id of "container" from the page
+console.log("\n----------------->Excercise 31 DOM<-----------------")
+   let divparagraph = document.getElementsByClassName('container')[0]
 //         32) Get every <td> element from the page
+console.log("\n----------------->Excercise 31 DOM<-----------------")
+    let tableData = document.querySelectorAll('td')
 //         33) Use a loop for printing the text inside of every <td> element in the page
+
 //         34) Write a function to change the heading of the page
 //         35) Write a function to add an extra row to the table
 //         36) Write a function to add a class of "test" to each row in the table
